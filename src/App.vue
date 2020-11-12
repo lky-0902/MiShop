e3e25c40aec89797feea6fce316235a017145f4c
lky-0902 <1047932703@qq.com>
2020-11-12 16:33:21 +0800

    nav-header组件

--- src/App.vue
+++ src/App.vue
@@ -29,12 +29,6 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import './assets/scss/reset.scss';
 
 </style>
