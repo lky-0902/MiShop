5a264ef8a201513b9d1beff222a67f6dcb240cb0
lky-0902 <1047932703@qq.com>
2020-12-02 11:48:21 +0800

    微信支付对接

--- src/pages/orderPay.vue
+++ src/pages/orderPay.vue
@@ -7,10 +7,8 @@
             <div class="icon-succ"></div>
             <div class="order-info">
               <h2>订单提交成功！去付款咯～</h2>
-              <p>
-                请在<span>30分</span>内完成支付, 超时后将取消订单
-              </p>
-              <p>收货信息：{{addressInfo}}</p>
+              <p>请在<span>30分</span>内完成支付, 超时后将取消订单</p>
+              <p>收货信息：{{ addressInfo }}</p>
             </div>
             <div class="order-total">
               <p>
@@ -19,26 +17,30 @@
               </p>
               <p>
                 订单详情
-                <em class="icon-down" :class="{'up':showDetail}" @click="showDetail = !showDetail"></em>
+                <em
+                  class="icon-down"
+                  :class="{ up: showDetail }"
+                  @click="showDetail = !showDetail"
+                ></em>
               </p>
             </div>
           </div>
           <div class="item-detail" v-if="showDetail">
             <div class="item">
               <div class="detail-title">订单号：</div>
-              <div class="detail-info theme-color">{{orderNo}}</div>
+              <div class="detail-info theme-color">{{ orderId }}</div>
             </div>
             <div class="item">
               <div class="detail-title">收货信息：</div>
-              <div class="detail-info">{{addressInfo}}</div>
+              <div class="detail-info">{{ addressInfo }}</div>
             </div>
             <div class="item good">
               <div class="detail-title">商品名称：</div>
               <div class="detail-info">
                 <ul>
-                  <li v-for="(item,index) in orderDetail" :key="index">
+                  <li v-for="(item, index) in orderDetail" :key="index">
                     <img v-lazy="item.productImage" />
-                    {{item.productName}}
+                    {{ item.productName }}
                   </li>
                 </ul>
               </div>
@@ -53,70 +55,108 @@
           <h3>选择以下支付方式付款</h3>
           <div class="pay-way">
             <p>支付平台</p>
-            <div class="pay pay-ali" :class="{'checked':payType==1}" @click="paySubmit(1)"></div>
-            <div class="pay pay-wechat" :class="{'checked':payType==2}" @click="paySubmit(2)"></div>
+            <div
+              class="pay pay-ali"
+              :class="{ checked: payType == 1 }"
+              @click="paySubmit(1)"
+            ></div>
+            <div
+              class="pay pay-wechat"
+              :class="{ checked: payType == 2 }"
+              @click="paySubmit(2)"
+            ></div>
           </div>
         </div>
       </div>
     </div>
+    <scan-pay-code v-if="showPay" @close="closePayModal" :img="payImg"></scan-pay-code>
   </div>
 </template>
 
 <script>
+import QRCode from 'qrcode'
+import ScanPayCode from './../components/ScanPayCode'
 export default {
-  name:'pay',
+  name: "pay",
   data() {
     return {
-      orderNo:this.$route.query.orderNo,
-      addressInfo:'', //收货人地址信息
-      orderDetail:[], //订单详情，包含了商品列表
-      showDetail:false, //是否显示订单详情
-      payType:'', //支付类型
-    }
+      orderId: this.$route.query.orderNo,
+      addressInfo: "", //收货人地址信息
+      orderDetail: [], //订单详情，包含了商品列表
+      showDetail: false, //是否显示订单详情
+      payType: "", //支付类型
+      showPay:false,//是否显示微信支付弹框
+      payImg:'', // 微信二维码
+    };
+  },
+  components:{
+    ScanPayCode
   },
   mounted() {
-    this.getOrderDetail()
+    this.getOrderDetail();
   },
   methods: {
-    getOrderDetail(){
-      this.axios.get(`/orders/${this.orderNo}`).then(res => {
-        let item = res.shippingVo
-        this.addressInfo = `${item.receiverName} ${item.receiverMobile} ${item.receiverProvince} ${item.receiverCity} ${item.receiverDistrict} ${item.receiverName} ${item.receiverAddress}`
-        this.orderDetail = res.orderItemVoList
-      })
+    getOrderDetail() {
+      this.axios.get(`/orders/${this.orderId}`).then((res) => {
+        let item = res.shippingVo;
+        this.addressInfo = `${item.receiverName} ${item.receiverMobile} ${item.receiverProvince} ${item.receiverCity} ${item.receiverDistrict} ${item.receiverName} ${item.receiverAddress}`;
+        this.orderDetail = res.orderItemVoList;
+      });
     },
-    paySubmit(payType){
-      if(payType == 1){
-        window.open('/#/order/alipay?orderId=' + this.orderNo,'_blank')
+    paySubmit(payType) {
+      if (payType == 1) {
+        this.payType = payType;
+        window.open("/#/order/alipay?orderId=" + this.orderId, "_blank");
+      } else if(payType == 2){
+        this.payType = payType;
+        this.axios
+          .post("/pay", {
+            orderId: this.orderId,
+            orderName: "Vue高仿小米商城",
+            amount: 0.01, //单位元
+            payType: 2, //1.支付宝 2.微信
+          })
+          .then((res) => {
+            QRCode.toDataURL(res.content).then(url =>{
+              this.showPay = true
+              this.payImg =url
+            }).catch(err => {
+              this.$message.error('微信二维码生成失败，请稍后重试')
+            })
+          });
       }
+    },
+    // 关闭微信弹框
+    closePayModal(){
+      this.showPay = false
     }
   },
-}
+};
 </script>
 
 <style lang="scss">
 @import "../assets/scss/config.scss";
 @import "../assets/scss/mixin.scss";
-.pay{
-  .wrapper{
+.pay {
+  .wrapper {
     background-color: $colorJ;
     padding-top: 30px;
     padding-bottom: 61px;
-    .order-wrap{
+    .order-wrap {
       padding: 62px 50px;
       background-color: $colorG;
       font-size: $fontJ;
       margin-bottom: 30px;
-      .item-order{
+      .item-order {
         display: flex;
         align-items: center;
-        .icon-succ{
+        .icon-succ {
           @include bgImg(90px, 90px, "/imgs/icon-gou.png", 60px);
           background-color: #80c58a;
           border-radius: 50%;
           margin-right: 40px;
         }
-        .order-info{
+        .order-info {
           margin-right: 248px;
           h2 {
             font-size: $fontE;
@@ -174,7 +214,7 @@ export default {
         }
       }
     }
-    .item-pay{
+    .item-pay {
       padding: 26px 50px 72px;
       background-color: $colorG;
       color: $colorB;
@@ -186,22 +226,22 @@ export default {
         border-bottom: 1px solid #d7d7d7;
         margin-bottom: 26px;
       }
-      .pay-way{
+      .pay-way {
         font-size: $fontH;
-        .pay{
+        .pay {
           display: inline-block;
           width: 188px;
           height: 64px;
           border: 1px solid $colorF;
           cursor: pointer;
-          &.last-child{
+          &.last-child {
             margin-left: 20px;
           }
-          &.checked{
+          &.checked {
             border: 1px solid $colorA;
           }
         }
-        .pay-ali{
+        .pay-ali {
           background: url("/imgs/pay/icon-ali.png") no-repeat center;
           background-size: 103px 38px;
           margin-top: 19px;
