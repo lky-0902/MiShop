930cdb0ad607554a83de3fa5d8d2c954938cb322
lky-0902 <1047932703@qq.com>
2020-12-03 16:46:44 +0800

    去除head标签中预加载的js文件，实现真正的按需加载

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,6 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../pages/home.vue'
 import Index from '../pages/index.vue'
-// import Product from '../pages/product.vue'
-// import Detail from '../pages/detail.vue'
-// import Order from '../pages/order.vue'
-// import OrderList from '../pages/orderList.vue'
-// import OrderConfirm from '../pages/orderConfirm.vue'
-// import OrderPay from '../pages/orderPay.vue'
-// import Login from '../pages/login.vue'
-// import Cart from '../pages/cart.vue'
-// import AliPay from '../pages/aliPay.vue'
 Vue.use(VueRouter)
 
 /**
@@ -38,12 +29,12 @@ const routes = [
       {
         path:'/product/:id',
         name:'product',
-        component:resolve => require(['../pages/product.vue'],resolve)
+        component:() => import('../pages/product.vue')
       },
       {
         path:'/detail/:id',
         name:'detail',
-        component:resolve => require(['../pages/detail.vue'],resolve)
+        component:() => import('../pages/detail.vue')
       }
     ]
   },
@@ -55,34 +46,34 @@ const routes = [
       {
         path:'list',
         name:'list',
-        component:() => import('../pages/orderList.vue'),
+        component:() => import('../pages/orderList.vue')
       },
       {
         path:'confirm',
         name:'confirm',
-        component:() => import('../pages/orderConfirm.vue'),
+        component:() => import('../pages/orderConfirm.vue')
       },
       {
         path:'pay',
         name:'pay',
-        component:() => import('../pages/orderPay.vue'),
+        component:() => import('../pages/orderPay.vue')
       },
       {
         path:'alipay',
         name:'alipay',
-        component:() => import('../pages/aliPay.vue'),
+        component:() => import('../pages/aliPay.vue')
       }
     ]
   },
   {
     path:'/login',
     name:'login',
-    component:resolve => require(['../pages/login.vue'],resolve)
+    component:() => import('../pages/login.vue')
   },
   {
     path:'/cart',
     name:'cart',
-    component:() => import('../pages/cart.vue'),
+    component:() => import('../pages/cart.vue')
   }
 ]
 
--- vue.config.js
+++ vue.config.js
@@ -17,6 +17,9 @@ module.exports = {
   //outputDir:'dist'   当运行 vue-cli-service build 时生成的生产环境构建文件的目录。
   //indexPath:'index.html'  指定生成的 index.html 的输出路径 (相对于 outputDir)。也可以是一个绝对路径。
   //lintOnSave:false   eslint的开关
-  productionSourceMap:false //如不需生产环境的sourcemap 可以将其设为false以加速生产环境构建,另外如果为true会直接暴露代码，不安全
-
+  productionSourceMap:false, //如不需生产环境的sourcemap 可以将其设为false以加速生产环境构建,另外如果为true会直接暴露代码，不安全
+  // 去除head标签中预加载的js文件，实现真正的按需加载
+  chainWebpack:(config) => {
+    config.plugins.delete('prefetch')
+  }
 }
