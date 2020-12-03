0a067b787ea74bdd41c271f669cf7f3082c883f1
lky-0902 <1047932703@qq.com>
2020-12-03 16:32:26 +0800

    路由按需加载方案，两种

--- src/router/index.js
+++ src/router/index.js
@@ -2,16 +2,26 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../pages/home.vue'
 import Index from '../pages/index.vue'
-import Product from '../pages/product.vue'
-import Detail from '../pages/detail.vue'
-import Order from '../pages/order.vue'
-import OrderList from '../pages/orderList.vue'
-import OrderConfirm from '../pages/orderConfirm.vue'
-import OrderPay from '../pages/orderPay.vue'
-import Login from '../pages/login.vue'
-import Cart from '../pages/cart.vue'
-import AliPay from '../pages/aliPay.vue'
+// import Product from '../pages/product.vue'
+// import Detail from '../pages/detail.vue'
+// import Order from '../pages/order.vue'
+// import OrderList from '../pages/orderList.vue'
+// import OrderConfirm from '../pages/orderConfirm.vue'
+// import OrderPay from '../pages/orderPay.vue'
+// import Login from '../pages/login.vue'
+// import Cart from '../pages/cart.vue'
+// import AliPay from '../pages/aliPay.vue'
 Vue.use(VueRouter)
+
+/**
+ * 路由懒加载方案一：
+ * component:resolve => require(['../pages/product.vue'],resolve)
+ * 路由懒加载方案二：
+ *  安装插件npm i @babel/plugin-syntax-dynamic-import --save
+ *  component:() => import('../pages/order.vue'),
+ */
+
+
 
 const routes = [
   {
@@ -28,51 +38,51 @@ const routes = [
       {
         path:'/product/:id',
         name:'product',
-        component:Product
+        component:resolve => require(['../pages/product.vue'],resolve)
       },
       {
         path:'/detail/:id',
         name:'detail',
-        component:Detail
+        component:resolve => require(['../pages/detail.vue'],resolve)
       }
     ]
   },
   {
     path:'/order',
     name:'order',
-    component:Order,
+    component:() => import('../pages/order.vue'),
     children:[
       {
         path:'list',
         name:'list',
-        component:OrderList
+        component:() => import('../pages/orderList.vue'),
       },
       {
         path:'confirm',
         name:'confirm',
-        component:OrderConfirm
+        component:() => import('../pages/orderConfirm.vue'),
       },
       {
         path:'pay',
         name:'pay',
-        component:OrderPay
+        component:() => import('../pages/orderPay.vue'),
       },
       {
         path:'alipay',
         name:'alipay',
-        component:AliPay
+        component:() => import('../pages/aliPay.vue'),
       }
     ]
   },
   {
     path:'/login',
     name:'login',
-    component:Login
+    component:resolve => require(['../pages/login.vue'],resolve)
   },
   {
     path:'/cart',
     name:'cart',
-    component:Cart
+    component:() => import('../pages/cart.vue'),
   }
 ]
 
